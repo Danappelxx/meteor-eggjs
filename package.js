8a57d4e83886127c2e1eb5fd8c540131d2826e60
Dan Appel <Dan.appel00@gmail.com>
2015-05-16 02:56:59 -0700

    update egg.js

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
 	name: "danappelxx:eggjs",
 	summary: "A wrapper egg.js to add easter eggs to web pages.",
-	version: "1.0.0",
+	version: "1.0.4",
 	git: "https://github.com/Danappelxx/meteor-eggjs.git",
 	documentation: "README.md"
 });
